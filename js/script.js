1abd8434981eea6409f821b6adce7e3cae0ca344
lavandil <aleivasav@gmail.com>
2017-01-21 21:24:44 +0300

    working with menu

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 /**
  * Created by admin on 21.01.2017.
  */
+
+var test = document.getElementsByClassName('main-menu');
+var navControl = document.getElementsByClassName('navbar-toggle');
+
+navControl[0].onclick = function (){
+    var mainMenuList = document.querySelectorAll('.main-menu > li');
+    for (var i = 0; i < mainMenuList.length; i++) {
+       var display = mainMenuList[i].style.display;
+
+        (display == "block") ? mainMenuList[i].style.display = "none" : mainMenuList[i].style.display = "block";
+    }
+}
