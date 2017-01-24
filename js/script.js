10aa7ef71d75c3d3b14909deaddd04db3161f7ca
lavandil <aleivasav@gmail.com>
2017-01-25 00:04:59 +0300

    menu ready

--- js/script.js
+++ js/script.js
@@ -8,8 +8,27 @@ var navControl = document.getElementsByClassName('navbar-toggle');
 navControl[0].onclick = function (){
     var mainMenuList = document.querySelectorAll('.main-menu > li');
     for (var i = 0; i < mainMenuList.length; i++) {
-       var display = mainMenuList[i].style.display;
+       // var display = mainMenuList[i].style.display;
+        var none = "none";
+       var li = mainMenuList[i];
+       var className = li.className;
+       var position;
+       if((position=className.indexOf(none)) ==-1) {
+           li.className += " " + none;
+       }
+       else {
+           li.className = (position==0)?
+               className.slice(none.length):
+               className.slice(0, position);
+           // if(position == 0){
+           //     className.slice(none.length);
+           // }
+           // else{
+           //     className.slice(0, position);
+           // }
+       }
 
-        (display == "block") ? mainMenuList[i].style.display = "none" : mainMenuList[i].style.display = "block";
+        // (display == "block") ? mainMenuList[i].style.display = "none" : mainMenuList[i].style.display = "block";
     }
 }
+//
