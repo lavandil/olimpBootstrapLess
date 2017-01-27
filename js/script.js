5d67408a1dc9d08307f3c304c6805c68e411bc2b
lavandil <aleivasav@gmail.com>
2017-01-27 23:23:03 +0300

    timer without animation

--- js/script.js
+++ js/script.js
@@ -2,33 +2,37 @@
  * Created by admin on 21.01.2017.
  */
 
-var test = document.getElementsByClassName('main-menu');
-var navControl = document.getElementsByClassName('navbar-toggle');
+(function() {
+    var test = document.getElementsByClassName('main-menu');
+    var navControl = document.getElementsByClassName('navbar-toggle');
 
-navControl[0].onclick = function (){
-    var mainMenuList = document.querySelectorAll('.main-menu > li');
-    for (var i = 0; i < mainMenuList.length; i++) {
-       // var display = mainMenuList[i].style.display;
-        var none = "none";
-       var li = mainMenuList[i];
-       var className = li.className;
-       var position;
-       if((position=className.indexOf(none)) ==-1) {
-           li.className += " " + none;
-       }
-       else {
-           li.className = (position==0)?
-               className.slice(none.length):
-               className.slice(0, position);
-           // if(position == 0){
-           //     className.slice(none.length);
-           // }
-           // else{
-           //     className.slice(0, position);
-           // }
-       }
-
-        // (display == "block") ? mainMenuList[i].style.display = "none" : mainMenuList[i].style.display = "block";
+    navControl[0].onclick = function () {
+        var mainMenuList = document.querySelectorAll('.main-menu > li');
+        for (var i = 0; i < mainMenuList.length; i++) {
+            // var display = mainMenuList[i].style.display;
+            var none = "none";
+            var li = mainMenuList[i];
+            var className = li.className;
+            var position;
+            if ((position = className.indexOf(none)) == -1) {
+                li.className += " " + none;
+            }
+            else {
+                li.className = (position == 0) ?
+                    className.slice(none.length) :
+                    className.slice(0, position);
+            }
+        }
     }
-}
-//
+
+})();
+
+$(function(){
+    var  test = $(".row:last-child");
+    // test.text(123);
+    $(".row:last-child").startTimer(
+        {
+          zeroDate: "2018-02-10"
+        }
+    );
+})
